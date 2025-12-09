0bb07e217ddecdc7de60acbfa0b70c43be09d79d
tensorqt <73964726+tomoqt@users.noreply.github.com>
2025-12-09 23:11:23 +0100

    Reduce batch size from 64 to 12

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -15,7 +15,7 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'shakespeare_char'
 gradient_accumulation_steps = 1
-batch_size = 64
+batch_size = 12
 block_size = 256 # context of up to 256 previous characters
 curvature = 1.0
 # baby GPT model :)
